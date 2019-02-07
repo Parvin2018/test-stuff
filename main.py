6a9030f645d7cd6f9b0da182a362ab08c34137c8
irzref <irzref.92@gmail.com>
2019-02-07 19:38:24 +0100

    update code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 
 # Converting names to numbers
 import random
-def name_to_number(name):
+def convert_name_to_number(name):
     if name == "rock":
         return 0
     elif name == "spock":
@@ -21,11 +21,8 @@ def name_to_number(name):
         return 3
     elif name == "scissors":
         return 4
-    else:
-        print "Hey! Wana paly? Pick something!"
-        return
 
-def number_to_name(number):
+def convert_number_to_name(number):
     if number == 0:
         return "rock"
     elif number == 1:
@@ -35,36 +32,47 @@ def number_to_name(number):
     elif number == 3:
         return "lizard"
     elif number == 4:
-        return "scissors"
-    else:
-        print "Error: choose another number!"
-        return
+        return "scissors"    
 
 
 
-def uplay():
-    player_choice = raw_input("Enter (rock, spock, paper, lizard, scissors): ")
-    # print out the message for the player's choice
-    print "Player chooses %s" % player_choice
-    # convert the player's choice to player_number using the function name_to_number()
-    player_number = name_to_number(player_choice)
-    # compute random guess for comp_number using random.randrange()
-    comp_number = random.randrange(0, 5)
-    # convert comp_number to comp_choice using the function number_to_name()
-    comp_choice = number_to_name(comp_number)
-    # print out the message for computer's choice
-    print "Computer chooses %s" % comp_choice
-    # compute difference of comp_number and player_number
-    diff = player_number - comp_number
-    if diff == 0:
-        print "You and I tie!"
-    elif diff == 4 or diff == 3 or diff == -1 or diff == -2:
-        print "I win!"
-    else:
-        print "You win!"
+def play(name_to_number, number_to_name):
+    player_choice = raw_input("Enter your choice (rock, spock, paper, lizard, scissors) or press enter to exit: ")
+    
+    if (player_choice == ''):
+        
+        print "Bye Bye"
+    
+    else :
+        
+        # print out the message for the player's choice
+        print "Player chooses %s" % player_choice
+        
+        # convert the player's choice to player_number using the function name_to_number()
+        player_number = name_to_number(player_choice)
+        
+        # compute random guess for comp_number using random.randrange()
+        comp_number = random.randrange(0, 5)
+        
+        # convert comp_number to comp_choice using the function number_to_name()
+        comp_choice = number_to_name(comp_number)
+        
+        # print out the message for computer's choice
+        print "Computer chooses %s" % comp_choice
+        
+        # compute difference of comp_number and player_number
+        evaluation = lambda a,b : a - b
+        result = evaluation(player_number, comp_number)
+
+        if (result == 0):
+            print "You and I tie!"
+        elif (result == 4) or (result == 3) or (result == -1) or (result == -2):
+            print "I win!"
+        else:
+            print "You win!"
 
 # testing the code
-uplay()
+play(convert_name_to_number, convert_number_to_name)
 
 
 
